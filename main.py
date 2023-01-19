fe7b4ddbcb3fb0fe29dd8487766fcf94a6b2e759
Asadbek <qurbonovasadbek@gmail.com>
2023-01-19 22:37:38 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+
+def filenamer():
+    custom_name = 'name.ext'
+    count = 1
+    if not os.path.exists(custom_name):
+        while True:
+            custom_name2 = 'name ({count}).ext'
+            if not os.path.exists(custom_name2):
+                return custom_name2.format(count=count)
+            else:
+                count += 1
