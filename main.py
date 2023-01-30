6680057a4478402dd4a13c9d1e2697a0ef362130
advenuz <qurbonovasadbek@gmail.com>
2023-01-30 18:57:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import os
 def filenamer():
     custom_name = 'name.ext'
     count = 1
-    if not os.path.exists(custom_name):
+    if os.path.exists(custom_name):
         while True:
             custom_name2 = 'name ({count}).ext'
-            if not os.path.exists(custom_name2):
+            if not os.path.exists(custom_name2.format(count=count)):
                 return custom_name2.format(count=count)
             else:
                 count += 1
